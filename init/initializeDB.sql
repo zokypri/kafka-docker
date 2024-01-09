9a811b597eb852f33c2930db035e0ebd4acf3db2
zpri <zoky.zp@gmail.com>
2024-01-09 11:53:33 +0700

    create DB, schema and table on initialization when running docker compose

--- init/initializeDB.sql
+++ init/initializeDB.sql
@@ -1,10 +1,15 @@
 
-USE AlertService;
+CREATE DATABASE IF NOT EXISTS NewsAgency;
 
-CREATE TABLE if not exists alerts (
+USE NewsAgency;
+
+CREATE SCHEMA if not exists ALERT_SERVICE;
+
+CREATE TABLE if not exists ALERTS (
                                       id BIGINT AUTO_INCREMENT PRIMARY KEY,
                                       alert_id BIGINT not null ,
                                       msg VARCHAR(255) NOT NULL,
     type VARCHAR(50) NOT NULL,
     level VARCHAR(50) NOT NULL
     );
+
